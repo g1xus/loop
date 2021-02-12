e2be6e53b761d25493e588c814aa2b5f17af02b6
g1xus <g1xus@yandex.ru>
2021-02-13 00:43:22 +0300

    Chats routing is done

--- src/components/Messenger/Chats/Chat.jsx
+++ src/components/Messenger/Chats/Chat.jsx
@@ -1,14 +1,15 @@
 import c from './Chat.module.css'
+import {NavLink} from "react-router-dom";
 
 function Chat(props) {
     return (
-        <div className={c.chat}>
+        <NavLink className={c.chat} to={`/messenger/${props.chatId}`}>
             <img src={props.chatImg} alt="chat image" className={c.chatImg}/>
             <div className={c.chatInfo}>
                 <div className={c.chatTitle}>{props.chatTitle}</div>
                 <div className={c.chatPreview}>{props.chatPreview}</div>
             </div>
-        </div>
+        </NavLink>
     );
 }
 
--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -5,10 +5,10 @@ function Messenger() {
     return (
         <main className={c.messenger}>
             <div className={c.chats}>
-                <Chat chatImg='https://sun7-9.userapi.com/NaDhheZgzqww8wt6JKAlUV2sHjRgo92UDLLNZg/v2y0jdIofS4.jpg' chatTitle='4135 без кураторов' chatPreview='Группа гэйэв'/>
-                <Chat chatImg='https://sun7-6.userapi.com/s/v1/ig2/IjV3I1ercAAEW67q0CilZpUXTUzYjMAL-kMcT5lJ5PvbNknCjIpDRGLnNSEpQ0iQN-ROYi3qAFXgzo7mTr8B-DN7.jpg?size=50x0&quality=96&crop=169,0,809,809&ava=1' chatTitle='Айнур Каюмов' chatPreview='У меня есть отврётка'/>
-                <Chat chatImg='https://sun7-9.userapi.com/DQmnSfi3fqY95GRDuNIWMKGOgFOgA5qp7lohgg/UHNZYwdALg8.jpg' chatTitle='Гарайшин Артур' chatPreview='Уважаемый человек'/>
-                <Chat chatImg='https://sun7-8.userapi.com/s/v1/ig2/ZRn6JP6VynD9fyHgmI5WLMZZoCfcClDgXYz_P6Mw6ZfnMkKJfyNlodGOtcKuoGImymybd25xQMS-szUZp19lrKz5.jpg?size=50x0&quality=96&crop=0,182,718,718&ava=1' chatTitle='Местный Дурачок' chatPreview='#freeNavalny'/>
+                <Chat chatImg='https://sun7-9.userapi.com/NaDhheZgzqww8wt6JKAlUV2sHjRgo92UDLLNZg/v2y0jdIofS4.jpg' chatTitle='4135 без кураторов' chatPreview='Группа гэйэв' chatId='1'/>
+                <Chat chatImg='https://sun7-6.userapi.com/s/v1/ig2/IjV3I1ercAAEW67q0CilZpUXTUzYjMAL-kMcT5lJ5PvbNknCjIpDRGLnNSEpQ0iQN-ROYi3qAFXgzo7mTr8B-DN7.jpg?size=50x0&quality=96&crop=169,0,809,809&ava=1' chatTitle='Айнур Каюмов' chatPreview='У меня есть отврётка' chatId='2'/>
+                <Chat chatImg='https://sun7-9.userapi.com/DQmnSfi3fqY95GRDuNIWMKGOgFOgA5qp7lohgg/UHNZYwdALg8.jpg' chatTitle='Гарайшин Артур' chatPreview='Уважаемый человек' chatId='3'/>
+                <Chat chatImg='https://sun7-8.userapi.com/s/v1/ig2/ZRn6JP6VynD9fyHgmI5WLMZZoCfcClDgXYz_P6Mw6ZfnMkKJfyNlodGOtcKuoGImymybd25xQMS-szUZp19lrKz5.jpg?size=50x0&quality=96&crop=0,182,718,718&ava=1' chatTitle='Местный Дурачок' chatPreview='#freeNavalny' chatId='4'/>
 
             </div>
             <div className={c.messages}>
